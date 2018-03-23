65a90b35801ae5675e1cbbe7d15e077a3fe02e6f
Evan <Evan@github.com>
2018-03-23 14:57:36 +0100

    better indent for compatibility

--- src/utilities/compatibility.py
+++ src/utilities/compatibility.py
@@ -5,13 +5,16 @@ class Compatibility:
 
     def are_compatible(self, person1, person2, person3=None):
         """Returns true if all persons are compatible to be in a relationship."""
-        persons = [person1, person2] if person3 is None else [
-            person1, person2, person3]
-        return self.are_different_persons(persons) and self.are_all_dating(persons) and self.are_sexually_compatible(
-            persons) and \
-               self.are_age_compatible(persons) and self.are_consanguinity_compatible(
-            persons) and self.are_compatible_if_minority(persons) and \
-               self.are_not_ex(persons) and self.are_throuple_compatible(persons)
+        persons = [person1, person2] if person3 is None else [person1, person2, person3]
+
+        return self.are_different_persons(persons) and \
+               self.are_all_dating(persons) and \
+               self.are_sexually_compatible(persons) and \
+               self.are_age_compatible(persons) and \
+               self.are_consanguinity_compatible(persons) and \
+               self.are_compatible_if_minority(persons) and \
+               self.are_not_ex(persons) and \
+               self.are_throuple_compatible(persons)
 
     def are_different_persons(self, persons):
         """Returns true if all persons are unique and not already set as each other's partners."""
